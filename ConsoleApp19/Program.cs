1de06737c480553c58b3949421a5775962d1bbaf
User_PC-Oren4 <User_PC-Oren4@PC-OREN4>
2025-03-05 23:15:29 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите размер массива:");
+int a = int.Parse(Console.ReadLine());
+int[] array = new int[a];
+for (int i = 0; i < a; i++)
+{
+    Console.WriteLine($"Введите злемент {i + 1}:");
+    array[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Содержимое массива:");
+foreach (int элемент in array)
+{
+    Console.WriteLine(элемент);
+}
